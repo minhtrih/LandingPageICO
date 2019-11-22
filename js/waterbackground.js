3e12d1c5ebbaca8e11a564757b584cc17d5278d5
Hoang Minh Tri <tri.hm@agiletech.vn>
2019-11-22 17:26:09 +0700

    update

--- js/waterbackground.js
+++ js/waterbackground.js
@@ -4,11 +4,20 @@
   document.addEventListener('DOMContentLoaded', function() {
     Typed.new('#typed', {
       stringsElement: document.getElementById('typed-strings'),
-      typeSpeed: 80,
+      typeSpeed: 50,
       startDelay: 1000,
       backDelay: 5000,
-      loop: true,
-      contentType: 'html' // or text
+      loop: false,
+      showCursor: true,
+      cursorChar: '|',
+      attr: null,
+      contentType: 'html', // or text
+      callback: function() {
+        var cursor = document.getElementsByClassName('typed-cursor')[0];
+        setTimeout(function() {
+          cursor.style.opacity = 0;
+        }, 2000);
+      }
     });
   });
 
